530baf6955af7cc041e6fa2685cc3dcf48841feb
programmerkim <jenniferkim.businessonly@gmail.com>
2020-04-11 19:08:03 -0700

    imports for getroute

--- routes/getroute.js
+++ routes/getroute.js
@@ -1,8 +1,3 @@
 const path = require('path');
 const fs = require('fs');
 
-module.exports = (app) => {
-    app.get("/api/notes", function (req, res) {
-        res.sendFile(path.join(__dirname, "../db/db.json"));
-    });
-};
