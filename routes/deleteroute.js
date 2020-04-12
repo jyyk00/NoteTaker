5cb1c0eaa77461937abb49985a3287b0ae0bcca5
programmerkim <jenniferkim.businessonly@gmail.com>
2020-04-11 19:08:53 -0700

    import for delte

--- routes/deleteroute.js
+++ routes/deleteroute.js
@@ -1,18 +1,2 @@
 const fs = require('fs');
 
-module.exports = (app) => {
-    app.delete("/api/notes/:id", function (req, res) {
-
-        let notes = fs.readFileSync("./db/db.json");
-
-        notes = JSON.parse(notes);
-
-        notes.splice(req, 1);
-
-        fs.writeFile("./db/db.json", JSON.stringify(notes), () => {
-            console.log("Note deleted.");
-        });
-
-        res.json(notes);
-    });
-};
